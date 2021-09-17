3bf32fd84bf54073307f07e12b4b4bf7b5baa1c7
Renilson89 <88753564+Renilson89@users.noreply.github.com>
2021-09-17 17:45:08 -0300

    Add files via upload

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+namespace Projeto_Aleatorio
+{
+  public abstract class EntidadeBase
+  {
+    public int id { get; protected set; }
+  }
+}
---
+++ Classes/Serie.cs
@@ -0,0 +1,56 @@
+using System;
+
+namespace Projeto_Aleatorio.Classes
+{
+  public class Serie : EntidadeBase
+  {
+
+    public Serie(Genero genero, string titulo, string descricao, int ano)
+    {
+      this.Genero = genero;
+      this.Titulo = titulo;
+      this.Descricao = descricao;
+      this.Ano = ano;
+
+    }
+    private Genero Genero { get; set; }
+    private string Titulo { get; set; }
+    private string Descricao { get; set; }
+    private int Ano { get; set; }
+    private bool Excluido { get; set; }
+
+    public Serie(int id, Genero Genero, string titulo, string Descricao, int ano)
+    {
+      this.id = id;
+      this.Genero = Genero;
+      this.Titulo = titulo;
+      this.Descricao = Descricao;
+      this.Ano = ano;
+      this.Excluido = false;
+    }
+
+    public override string ToString()
+    {
+      string retorno = "";
+      retorno += "Gênero:" + this.Genero + Environment.NewLine;
+      retorno += "Titulo:" + this.Titulo + Environment.NewLine;
+      retorno += "Descrição:" + this.Descricao + Environment.NewLine;
+      retorno += "Ano de Inicio" + this.Ano + Environment.NewLine;
+      retorno += "Excluido:" + this.Excluido;
+      return retorno;
+    }
+    public string retornaTitulo()
+    {
+      return this.Titulo;
+    }
+    public int retornaId()
+    {
+      return this.id;
+    }
+    public void Excluir()
+    {
+      this.Excluido = true;
+    }
+
+  }
+}
